1321e8d67dea34923912e854d551e72addf8fb17
Juan Pablo Perez Loaiza <152454724+pabloDarkmoon24@users.noreply.github.com>
2026-05-06 15:54:27 -0500

    fix: add fonts.googleapis.com to CSP style-src

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(
       directives: {
         defaultSrc:              ["'self'"],
         scriptSrc:               ["'self'"],
-        styleSrc:                ["'self'", "'unsafe-inline'"],
+        styleSrc:                ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
         imgSrc:                  ["'self'", 'data:', 'https:'],
         fontSrc:                 ["'self'", 'https://fonts.gstatic.com'],
         connectSrc:              ["'self'"],
